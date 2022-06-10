1469a3aa070cb7e72a760bd8a50168597d05ef13
Patrick Holton <holtonpk@gmail.com>
2022-06-10 17:41:23 -0600

    fonts

--- Clock2.py
+++ Clock2.py
@@ -57,7 +57,7 @@ class Display:
         # date = data['date']
 
         # Ttxt = graphics.DrawText(self.offscreen_canvas, self.font, 10, 15, graphics.Color(255, 255, 255), str("3:33"))
-        Dtxt = graphics.DrawText(self.offscreen_canvas, self.font2,  18, 13, graphics.Color(255, 255, 255), str("June 10"))
+        Dtxt = graphics.DrawText(self.offscreen_canvas, self.font2,  16, 15, graphics.Color(255, 255, 255), str("June 10"))
 
 
         # self.offscreen_canvas.Clear()
